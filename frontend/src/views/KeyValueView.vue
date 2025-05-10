14c6dd25afe309e5576f03b97a88b04b0cda3be5
GONCALOUNI <a76967@ualg.pt>
2025-05-10 16:27:56 +0100

    construcao do frontend muito basico

--- frontend/src/views/KeyValueView.vue
+++ frontend/src/views/KeyValueView.vue
@@ -1,6 +1,6 @@
 <template>
     <div class="p-6 space-y-4">
-      <h1 class="text-2xl font-semibold">Gestor de Chaves-Valor</h1>
+      <h1 class="text-2xl font-semibold">Gestor de Key-Value</h1>
   
       <div class="grid grid-cols-1 md:grid-cols-2 gap-4">
         <div>
