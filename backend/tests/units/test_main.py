3d7d29cc08c47558e0e1b04d6dcf8596c9c4c971
GONCALOUNI <a76967@ualg.pt>
2025-05-22 18:54:55 +0100

    yamls corrigidos

--- backend/tests/units/test_main.py
+++ backend/tests/units/test_main.py
@@ -6,6 +6,7 @@ from main import app, metadata, engine, db
 
 client = TestClient(app)
 
+
 @pytest.fixture(autouse=True, scope="function")
 def setup_db():
     metadata.drop_all(bind=engine)
@@ -26,13 +27,14 @@ def setup_db():
     loop.run_until_complete(db.disconnect())
     loop.close()
 
+
 def test_health():
     r = client.get("/health")
     assert r.status_code == 200
     assert r.json() == {"status": "ok"}
 
+
 def test_put_get_delete():
-    
     # PUT
     r = client.put("/kv", json={"key": "foo", "value": "bar"})
     assert r.status_code == 200 and r.json() == {"status": "ok"}
